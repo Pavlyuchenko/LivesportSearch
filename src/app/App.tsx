6a729694cdeea7e0d30a5375950d30a2a67517d7
Michal Pavlíček <michaelg.pavlicek@gmail.com>
2025-05-17 14:57:38 +0200

    Fix CSS class in App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import "./App.module.css";
 
 import SearchComponent from "../components/SearchComponent";
+import styles from "./App.module.css";
 
 function App() {
     return (
-        <main>
+        <main className={styles["app-container"]}>
             <SearchComponent />
         </main>
     );
