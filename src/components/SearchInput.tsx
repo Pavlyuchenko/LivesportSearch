1dd8d032de0c97ac8f7aae8294f66d56b3edbb12
Michal Pavlíček <michaelg.pavlicek@gmail.com>
2025-05-17 14:56:10 +0200

    Changes for Vercel deployment

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import styles from "./styles/SearchInput.module.css";
 import searchIcon from "@assets/search-icon.svg";
 import cancelButton from "@assets/cancel.svg";
